cc8b83c4755d074dedaa4ccc6b527cea1ef418d5
yeeun <dpdms9621@naver.com>
2023-06-12 19:23:41 +0900

    [feat] 각종 메시지 구현

---
+++ src/main/java/pairmatching/message/MatchingMessage.java
@@ -0,0 +1,16 @@
+package pairmatching.message;
+
+public class MatchingMessage {
+
+    private MatchingMessage() {}
+
+    public static String SELECT_FUNCTION_MESSAGE = "기능을 선택하세요.";
+    public static String SHARP_MESSAGE = "#############################################";
+    public static String CLASS_MESSAGE = "과정 : ";
+    public static String MISSION_MESSAGE = "미션 : ";
+    public static String SELECT_OPTION_MESSAGE = "과정, 레벨, 미션을 선택하세요.\n"+"ex) 백엔드, 레벨1, 자동차경주";
+    public static String ALREADY_INFORMATION_MESSAGE = "매칭 정보가 있습니다. 다시 매칭하시겠습니까?\n"+"네 | 아니오";
+    public static String MATCHING_RESULT_MESSAGE = "페어 매칭 결과입니다.";
+    public static String RESET_MESSAGE = "초기화 되었습니다.";
+
+}
