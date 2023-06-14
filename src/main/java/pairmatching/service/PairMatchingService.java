2cfd3d15a06d2bc2b554521fb989b845606eb72e
yeeun <dpdms9621@naver.com>
2023-06-14 23:18:25 +0900

    [feat] 파일 읽어서 List에 넣기

--- src/main/java/pairmatching/service/PairMatchingService.java
+++ src/main/java/pairmatching/service/PairMatchingService.java
@@ -1,13 +1,20 @@
 package pairmatching.service;
 
 import camp.nextstep.edu.missionutils.Console;
+import pairmatching.ReadCrewFile;
+import pairmatching.domain.ClassType;
 import pairmatching.view.MatchingView;
+
+import java.util.List;
 
 public class PairMatchingService {
 
     private MatchingView matchingView;
+    private ReadCrewFile readCrewFile;
+    List<String> crewNames; // 파일에서 로드한 크루 이름 목록
 
     public PairMatchingService() {
+        this.readCrewFile = new ReadCrewFile();
         this.matchingView = new MatchingView();
     }
 
@@ -30,6 +37,13 @@ public class PairMatchingService {
         String input = Console.readLine();
         // TODO : input에 따른 기능 구현
     }
+
+    // 파일 읽는 기능
+    public void readCrewFile() {
+        crewNames = readCrewFile.readFile(ClassType.BACK_END);
+        // TODO : 매개변수 동적으로 코드 작성 필요
+    }
+
 
 
 }
